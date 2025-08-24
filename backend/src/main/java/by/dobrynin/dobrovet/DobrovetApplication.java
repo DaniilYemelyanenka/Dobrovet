e5716a613fe6126470b722af8d714038a7e8ef7e
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-08-24 22:22:06 +0300

    create: create project

---
+++ backend/src/main/java/by/dobrynin/dobrovet/DobrovetApplication.java
@@ -0,0 +1,13 @@
+package by.dobrynin.dobrovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DobrovetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DobrovetApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/by/dobrynin/dobrovet/DobrovetApplicationTests.java
@@ -0,0 +1,13 @@
+package by.dobrynin.dobrovet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DobrovetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
